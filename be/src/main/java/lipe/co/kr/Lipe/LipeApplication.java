ec9f6ae38eabb937f9313958b7368bef40732967
Dachae <no3yho2@naver.com>
2025-04-10 10:07:35 +0900

    init: backend 프로젝트 생성

---
+++ be/src/main/java/lipe/co/kr/Lipe/LipeApplication.java
@@ -0,0 +1,13 @@
+package lipe.co.kr.Lipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LipeApplication.class, args);
+	}
+
+}
---
+++ be/src/test/java/lipe/co/kr/Lipe/LipeApplicationTests.java
@@ -0,0 +1,13 @@
+package lipe.co.kr.Lipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
